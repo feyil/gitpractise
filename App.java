2854c1361767266b4f86cfd177359be5dc6c003f
fey <furkan54emre@gmail.com>
2018-01-22 22:44:27 +0300

    start

---
+++ App.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class App {
+
+    public static void main(String[] args) {
+
+	System.out.println("Hello");
+	//Add commit 1
+	//Add commit 2
+
+	//Add person def
+	//Addd getName()
+    }
+}
